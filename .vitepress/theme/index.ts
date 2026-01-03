2ef43491b059d872d438e08a26cc6bb4c0f1a733
bertreyking <bertreyking@wbing.local>
2026-01-03 17:53:11 +0800

    update

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,6 +1,13 @@
 // .vitepress/theme/index.ts
 import Teek from "vitepress-theme-teek";
 import "vitepress-theme-teek/index.css";
+import "vitepress-theme-teek/theme-chalk/tk-doc-h1-gradient.css";
+import "vitepress-theme-teek/theme-chalk/tk-code-block-mobile.css";
+import "vitepress-theme-teek/theme-chalk/tk-sidebar.css";
+import "vitepress-theme-teek/theme-chalk/tk-aside.css";
+import "vitepress-theme-teek/theme-chalk/tk-nav.css";
+import "vitepress-theme-teek/theme-chalk/tk-doc-h1-gradient.css";
+import "vitepress-theme-teek/theme-chalk/tk-doc-fade-in.css";
 
 export default {
   extends: Teek,
